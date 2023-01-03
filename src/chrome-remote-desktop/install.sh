f4201d87f63094710cdb7bb56101edc20aa098c1
Renya <49476274+Renya-Kujirada@users.noreply.github.com>
2023-01-03 18:56:41 +0900

    Add files via upload

---
+++ src/chrome-remote-desktop/install.sh
@@ -0,0 +1,7 @@
+# see below link
+# https://support.google.com/chrome/answer/1649523#linux-crd&zippy=%2Clinux-%E3%81%A7-chrome-%E3%83%AA%E3%83%A2%E3%83%BC%E3%83%88-%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb
+rm chrome-remote-desktop_current_amd64.deb
+
---
+++ src/chrome-remote-desktop/uninstall.sh
@@ -0,0 +1,4 @@
+sudo systemctl stop chrome-remote-desktop.service
+sudo apt-get purge chrome-remote-desktop
+# sudo rm -r ~/.config/chrome-remote-desktop/
+sudo reboot
